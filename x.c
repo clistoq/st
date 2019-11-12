6db237552e7b7a51504a6dd7aaebb9489a538f5a
Dawid Butrykowski <butrykowski.dawid@gmail.com>
2019-11-13 00:21:41 +0100

    Reapply hidecursor patch

--- x.c
+++ x.c
@@ -734,6 +734,13 @@ brelease(XEvent *e)
 void
 bmotion(XEvent *e)
 {
+	if (!xw.pointerisvisible) {
+		XDefineCursor(xw.dpy, xw.win, xw.vpointer);
+		xw.pointerisvisible = 1;
+		if (!IS_SET(MODE_MOUSEMANY))
+			xsetpointermotion(0);
+	}
+
 	if (IS_SET(MODE_MOUSE) && !(e->xbutton.state & forcemousemod)) {
 		mousereport(e);
 		return;
